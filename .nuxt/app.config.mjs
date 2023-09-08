f97710df13c2706f77c29667b36342fb1b396d54
esmailza <esmailza@unlv.nevada.edu>
2023-09-08 16:39:40 -0700

    push

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from 'C:/Users/17028/OneDrive/Documents/GitHub/mina-resume.github.io/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
