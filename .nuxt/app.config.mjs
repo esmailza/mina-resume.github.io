338b0fddd987a8075e4998e8cd0c9cfadfe8aea6
esmailza <esmailza@unlv.nevada.edu>
2023-09-08 16:42:22 -0700

    delete

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from 'C:/Users/17028/OneDrive/Documents/GitHub/mina-resume.github.io/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
